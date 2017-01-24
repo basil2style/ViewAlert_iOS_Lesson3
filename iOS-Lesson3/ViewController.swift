62739d18f7b02f831399706ec1cb9be7a706b78a
Basil <basiltalias92@gmail.com>
2017-01-24 18:53:18 -0500

    Initial Commit

---
+++ iOS-Lesson3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Lesson3
+//
+//  Created by Basil on 2017-01-24.
+//  Copyright © 2017 Basil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
